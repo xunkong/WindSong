1ae61a0f49a9b8fb699bbba20f3973c5d59cea32
Scighost <scighost@outlook.com>
2023-02-13 19:56:36 +0800

    AdminHelper

---
+++ WindSong/Helpers/AdminHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Principal;
+
+namespace WindSong.Helpers;
+
+internal abstract class AdminHelper
+{
+
+
+    public static bool IsAdmin()
+    {
+        using WindowsIdentity identity = WindowsIdentity.GetCurrent();
+        WindowsPrincipal principal = new WindowsPrincipal(identity);
+        return principal.IsInRole(WindowsBuiltInRole.Administrator);
+    }
+
+
+
+    public static void RestartAsAdmin()
+    {
+        try
+        {
+            var file = Process.GetCurrentProcess().MainModule?.FileName;
+            if (string.IsNullOrWhiteSpace(file))
+            {
+                file = Path.Join(AppContext.BaseDirectory, "WindSong.exe");
+            }
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = file,
+                UseShellExecute = true,
+                Verb = "runas",
+            });
+        }
+        catch (Win32Exception ex)
+        {
+            if (ex.NativeErrorCode == 1223)
+            {
+                // ERROR_CANCELLED
+                // The operation was canceled by the user.
+            }
+            else
+            {
+                throw;
+            }
+        }
+    }
+
+
+
+}
